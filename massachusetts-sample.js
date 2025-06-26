160dc656df9483534dffb53c0ab5637155cf646d
ahmedsalam651 <alshamariahmed5@gmail.com>
2025-06-27 00:32:20 +0300

    Add files via upload

---
+++ massachusetts-sample.js
@@ -0,0 +1,242 @@
+const defaultMCQs = [
+  {
+    "q": "Whic h of the following statements about the nor-\nmal microbial f lor a is f alse?",
+    "a": "The microorganisms o n the e pithelial surf aces\n\nof the body remain in place primarily through \n\nadherence.",
+    "b": "Humans possess a large multitude of micro-\n\norganisms o n the skin, gastrointestinal (GI),\n\nva\nginal, and upper respir atory tr acts.",
+    "c": "There is little benefit of these microorganisms\n\nto humans.",
+    "d": "Severe infectio ns can result from the nor mal",
+    "correct": ""
+  },
+  {
+    "q": "Whic h of the following statements about\n\nStaphylococcus\nis tr ue?",
+    "a": "S.\n epider midis\nis found o n \n\u0001\n50% of skin \ncultures.",
+    "b": "V\nancomycin is the dr ug of c hoice for \nStaphylococcus\n-infected prosthetic hear t valves.",
+    "c": "Acute staphylococcal infectio ns r arely require\n\nintr avenous (IV) ther apy.",
+    "d": "Of the ter tiary care isolates of",
+    "correct": ""
+  },
+  {
+    "q": "Whic h of the following microorganisms is the most\n\ncommo n cause of prosthetic hear t valve infectio ns?",
+    "a": "Staphylococcus epider midis\n.",
+    "b": "Staphylococcus viridans\n.\n1\nGener al Medicine\n\u0001 \nQuestio ns",
+    "c": "Staphylococcus aureus\n.",
+    "d": "Haemophilus influenzae",
+    "correct": ""
+  },
+  {
+    "q": "Whic h of the following patients would not re-\n\nquire endocarditis prophylaxis during invasive\n\nsurgery?",
+    "a": "a patient with a prosthetic hear t valve.",
+    "b": "a patient with a previous history of bacterial\n\nendocarditis.",
+    "c": "a patient with mitr al valve prolapse with\n\nv\nalvular re gurgitatio n.",
+    "d": "a patient with a history of severe coro nary",
+    "correct": ""
+  },
+  {
+    "q": "What type of ocular surgery would require endo-\ncarditis prophylaxis?",
+    "a": "catar act surgery.",
+    "b": "vitrectomy.",
+    "c": "orbital f loor implant.",
+    "d": "tear duct reco nstr uctio n.",
+    "correct": ""
+  },
+  {
+    "q": "Whic h microorganism is commo nly associated\n\nwith pseudomembr anous colitis?",
+    "a": "Enterococcus\n.",
+    "b": "Escherichia coli.",
+    "c": "Clostridium difficile.",
+    "d": "Staphylococcus epider midis.",
+    "correct": ""
+  },
+  {
+    "q": "Whic h of the following statements is f alse?",
+    "a": "Go nococci are not par t of the nor mal micro-",
+    "b": "ial f lor a.\nb.\nFifty percent of women and 95% of men in-\n\nfected with go nococci are symptomatic.",
+    "c": "Chlamydia trachomatis \nis found in as many as\n\nhalf of all women and o ne third of all men \n\ninfected with go nococci.",
+    "d": "Macrolides and quinolo nes are gener ally",
+    "correct": ""
+  },
+  {
+    "q": "Whic h of the following statements about syphilis\n\nis f alse?",
+    "a": "The treatment of c hoice for patients with neu-\n\nrosyphilis is penicillin G, 2.4 millio n units \n\nintr amuscularly weekly for 3 weeks.",
+    "b": "Tr\nansplacental tr ansmissio n from an untreated,\npre gnant female patient to her fetus before 16\n\nw\neeks gestatio n can result in co ngenital\nsyphilis.",
+    "c": "The c lassic gummas of ter tiary syphilis are\n\nc\nhar acteriz ed by caseating g r anulomas.",
+    "d": "In neurosyphilis, the ser um venereal disease",
+    "correct": ""
+  },
+  {
+    "q": "Whic h of the following spiroc hetes causes Lyme\n\ndisease?",
+    "a": "T\nreponema pallidum.",
+    "b": "Ixodes scapularis.",
+    "c": "Bor relia bur gdorferi.",
+    "d": "Leptospira inter rogans.",
+    "correct": ""
+  },
+  {
+    "q": "Whic h of the following def ines a positive purified\n\nprotein derivative (PPD) reactio n in an immuno-\n\ncompetent individual?",
+    "a": "indur atio n of 10 mm or g reater, read 24 hour s\n\nlater.",
+    "b": "indur atio n of 10 mm or g reater, read 48 to \n\n72 hour s later.",
+    "c": "indur atio n of 5 mm or g reater, read 24 hour s\n\nlater.",
+    "d": "indur atio n of 5 mm or g reater, read 48 to",
+    "correct": ""
+  },
+  {
+    "q": "Whic h of the following is not a her pesvir us?",
+    "a": "human immunodeficienc y vir us (HIV).",
+    "b": "c\nytome galovir us (CMV).",
+    "c": "va\nricella z oster vir us (VZV).",
+    "d": "her pes simplex vir us (HSV).",
+    "correct": ""
+  },
+  {
+    "q": "Whic h of the following methods of tr ansmissio n\nof he patitis vir uses is incor rect?",
+    "a": "he patitisA: fecal\u00d0or al route.",
+    "b": "he patitis B: fecal\u00d0or al route and sexual tr ans-\n\nmissio n.",
+    "c": "he patitis C: blood tr ansfusio ns.",
+    "d": "he patitis D: sexual tr ansmissio n.",
+    "correct": ""
+  },
+  {
+    "q": "Whic h of the following statements aboutacquired\n\nimmunodeficienc y syndrome (AIDS) is tr ue?",
+    "a": "A\npproximately 500 millio n individuals in the\nw\norld have AIDS.",
+    "b": "Most individuals infected with human im-\n\nm\nunodeficienc y vir us (HIV) live in the\nUnited States.",
+    "c": "The enzyme-linked immunosorbent assay\n\n(ELISA) test for HIV is 99% sensitive but\n\non\nly\n \n50% specific for HIV.",
+    "d": "Infected breast milk can also be a method of",
+    "correct": ""
+  },
+  {
+    "q": "Causes of seco ndary hyper tensio n inc lude all of\n\nthe following exce pt:",
+    "a": "pheoc hromoc ytoma.",
+    "b": "pseudotumor cere bri.",
+    "c": "hyper aldostero nism.",
+    "d": "polyc ystic kidney disease.",
+    "correct": ""
+  },
+  {
+    "q": "Whic h medicatio n is incor rectly paired with its\n\ncommo n side effect?",
+    "a": "doxaz osin: postur al hypotensio n.",
+    "b": "a\ntenolol: bro nc hospasm.",
+    "c": "spiro nolacto ne: g ynecomastia.",
+    "d": "minoxidil: hir sutism.",
+    "correct": ""
+  },
+  {
+    "q": "What types of antihyper tensives should be used\n\nf\nor initial ther apy for hyper tensio n in newly diag-\nnosed patients?",
+    "a": "beta-bloc ker s and diuretics.",
+    "b": "calcium c hannel bloc ker s and angiotensin-",
+    "c": "o nver ting enzyme (ACE) inhibitor s.\nc.\nangiotensin II rece ptor bloc ker s.",
+    "d": "alpha-ago nists.",
+    "correct": ""
+  },
+  {
+    "q": "In the order of decreasing frequenc y, what are\n\nthe most commo n causes of death in the United\n\nStates?",
+    "a": "1. stroke, 2. cancer, 3. hear t disease, 4. acci-\n\ndents,5. diabetes.",
+    "b": "1. cancer, 2. hear t disease, 3. stroke, 4. dia-\n\nbetes, 5. c hro nic lower respir atory diseases\n\n(e.g., c hro nic obstr uctive pulmo nary disease\n\n[COPD]).",
+    "c": "1. hear t disease, 2. stroke, 3. cancer, 4. COPD,\n\n5. diabetes.",
+    "d": "1. hear t disease, 2. cancer, 3. stroke, 4. COPD,",
+    "correct": ""
+  },
+  {
+    "q": "accidents.\ne.\n1. hear t disease, 2. cancer, 3. stroke, 4. acci-\n\ndents, 5. COPD.\nQuestions\n3\n1\nJageCh01ff.qxd  8/26/05  4:06 PM  Page 3\n\n25.\nWhat is the definitio n of a tr ansient isc hemic",
+    "a": "ttac k (TIA)?\na.\na loss of neurologic functio n caused by is-\n\nc\nhemia that lasts for \n\u0001\n24 hour s and that\nc\nlear s without residual signs.",
+    "b": "a loss of neurologic functio n that lasts \n\nf\nor \n\u0002\n24 hour s, but for \n\u0001\n7 days, and that\nleavesnolastingsymptomsorsigns.",
+    "c": "a loss of neurologic functio n that leaves a per-\n\nsistent minor disability but does not prog ress\n\nto a major stroke.",
+    "d": "an isc hemic attac k, but not inf arcted area of",
+    "correct": ""
+  },
+  {
+    "q": "Whic h of the following statements about stroke \n\nis f alse?",
+    "a": "Clopidog rel has a better side effect profile\n\nthan tic lopidine with re gard to bo ne mar row\n\nsuppressio n.",
+    "b": "The use of tissue plasminogen activator\n\n(TPA) within 24 hour s of the o nset of\n\nsymptoms improves outcome in patients\n\nwith stroke.",
+    "c": "Carotid endar terectomy (CEA) is beneficial in\n\nreducing the incidence of stroke in sympto-\n\nmatic patients with carotid stenosis of\n\n\u0002\n70%.",
+    "d": "Gener aliz ed seizures are commo n in patients",
+    "correct": ""
+  },
+  {
+    "q": "F\nolic acid can reduce the risk of stroke in patients\nby\n \nreducing the plasma levels of:",
+    "a": "high-density lipoprotein (HDL) c holesterol.",
+    "b": "homoc ysteine.",
+    "c": "hemoglobin.",
+    "d": "triglycerides.",
+    "correct": ""
+  },
+  {
+    "q": "Whic h of the following statements about carotid\n\nendar terectomy (CEA) is f alse?",
+    "a": "CEA should o nly be co nsidered if the surgeo n\n\nperfor ming the oper atio n has a perioper ative\n\nmorbidity r ate of\n\u0001\n3%.",
+    "b": "In the Asymptomatic Carotid Atherosc lerosis\n\nStudy, patients with asymptomatic carotid\n\nstenosis of\n\u0002\n60% who underwent CEA did\nnot show a significantly lower risk of ha\nving\nanother major isc hemic stroke, compared to pa-\n\ntients who were solely o n medicatio n.",
+    "c": "Asymptomatic carotid br uits occur in 10% of\n\nthe populatio n older than 40 year s.",
+    "d": "In the Nor thAmerican Symptomatic Carotid",
+    "correct": ""
+  },
+  {
+    "q": "All of the following will increase the demand for\n\nox\ny\ngen by the myocardium exce pt:",
+    "a": "increased hear t r ate.",
+    "b": "decreased ventricular co ntr actility.",
+    "c": "increased ventricular wall thic kness.",
+    "d": "increased stroke volume.",
+    "correct": ""
+  },
+  {
+    "q": "51.\nW\nithin 6 to 12 hour s after a suspected inf arct,\nw\nhic h of the following is the most sensitive\nmarker for inf arct?",
+    "a": "tropo nins I andT and creatinine kinase MB.",
+    "b": "lactate dehydrogenase.",
+    "c": "erythroc yte sedimentatio n r ate (ESR).",
+    "d": "C-reactive protein.",
+    "correct": ""
+  },
+  {
+    "q": "P\nercutaneous tr ansluminal coro nary angioplasty\n(PTCA) is usually superior to thrombolytic ther-",
+    "a": "p\ny in acute coro nary syndromes in all of the fol-\nlowing situatio ns exce pt:\na.\ninitiatio n of ther apy \n\u0002\n90 minutes after acute\ncoro nary syndrome.",
+    "b": "cardiogenic shoc k.",
+    "c": "patients with an increased risk of intr acr anial\n\nhemor rhage.",
+    "d": "patients with a prior history of coro nary",
+    "correct": ""
+  },
+  {
+    "q": "Clinical signs of acute left-ventricular hear t f ail-\n\nure can inc lude all of the following exce pt:",
+    "a": "use of accessory musc les for respir atio n.",
+    "b": "hemoptysis.",
+    "c": "diaphoresis.",
+    "d": "mental status c hanges.",
+    "correct": ""
+  },
+  {
+    "q": "Causes of left-ventricular hyper trophy inc lude:",
+    "a": "systemic hyper tensio n.",
+    "b": "idiopathic hyper trophic subaor tic stenosis.",
+    "c": "coarctatio n of the aor ta.",
+    "d": "all of the above.",
+    "correct": ""
+  },
+  {
+    "q": "The afterload for the left ventric le is deter mined\nby  ____________,  w\nhereas the afterload for the\nright ventric le is deter mined by \n____________.",
+    "a": "left atrial pressure; right atrial pressure.",
+    "b": "mean ar terial pressure; mean venous pressure.",
+    "c": "pulmo nary venous pressure; inferior vena cava\n\npressure.",
+    "d": "aor tic pressure; pulmo nary ar tery pressure.",
+    "correct": ""
+  },
+  {
+    "q": "The most commo n cause of right-sided hear t\n\nf",
+    "a": "ilure is:\na.\nleft-sided hear t f ailure.",
+    "b": "primary pulmo nary hyper tensio n.",
+    "c": "por tal hyper tensio n.",
+    "d": "coarctatio n of the aor ta.",
+    "correct": ""
+  },
+  {
+    "q": "Usually, the most effective way of treating sys-\n\ntolic dysfunctio n is to:",
+    "a": "reduce preload.",
+    "b": "increase preload.",
+    "c": "reduce afterload.",
+    "d": "increase afterload.",
+    "correct": ""
+  },
+  {
+    "q": "The management of diastolic dysfunctio n can be",
+    "a": "ccomplished by:\na.\nreducing preload.",
+    "b": "reducing co ntr actility.",
+    "c": "increasing preload.",
+    "d": "increasing afterload.",
+    "correct": ""
+  }
+];
