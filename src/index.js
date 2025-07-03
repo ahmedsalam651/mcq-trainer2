ac55dedb45f951d0a8314c9d311645a9fbf95d77
ahmedsalam651 <alshamariahmed5@gmail.com>
2025-07-03 17:04:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js');
-  });
+  navigator.serviceWorker.register('/service-worker.js');
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
