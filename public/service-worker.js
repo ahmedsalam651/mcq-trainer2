f71624e728ef19afa2f285ca4d80a74dc7bd8e5d
ahmedsalam651 <alshamariahmed5@gmail.com>
2025-07-03 17:03:45 +0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,18 +1,15 @@
+const CACHE = 'mcq-pwa-v2';
+
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('mcq-pwa-v2').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/styles.css'
-      ]);
-    })
+    caches.open(CACHE).then(cache =>
+      cache.addAll(['./', './index.html', './manifest.json'])
+    )
   );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
